b5712187cf12049e0bc574ca2b7013cb1499b1c1
Casey Carroll <caseydean8@gmail.com>
2019-12-06 09:41:56 -0500

    added background image that scales to all screen sizes

--- bootstrap-portfolio2/src/App.js
+++ bootstrap-portfolio2/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import About from "./Components/About"
-import "./Components/content.css"
+import About from "./Components/About";
+import "./Components/content.css";
 
 function App() {
   return (
---
+++ bootstrap-portfolio2/src/Components/Background.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <div className="bg">
+      
+    </div>
+  )
+}
+
+export default Background
