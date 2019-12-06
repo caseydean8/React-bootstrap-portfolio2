c7f772db7dad9d932a291eb4a0a65c8e4f66f40c
Casey Carroll <caseydean8@gmail.com>
2019-12-05 19:03:42 -0500

    added links and colorhunt pallete 79325

--- bootstrap-portfolio2/src/App.js
+++ bootstrap-portfolio2/src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- bootstrap-portfolio2/src/Components/Navbar.js
+++ bootstrap-portfolio2/src/Components/Navbar.js
@@ -1,34 +1,34 @@
 import React from "react";
-import Link from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav
       class="navbar navbar-expand-lg navbar-light"
-      style={{ backgroundColor: "#E3F2FD" }}
+      style={{ backgroundColor: "#c0c5cd" }}
     >
       <div class="container navbar-content">
         <span class="navbar-brand mb-0 h1">Casey Carroll</span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <Link class="nav-link" href="#" />
+          About
+          <Link class="nav-link" href="#" />
+          Portfolio
+          <Link class="nav-link" href="#" />
+          Contact
+        </div>
       </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <Link class="nav-link" href="#" />Home <span class="sr-only">(current)</span>
-      </li>
-      <li class="nav-item">
-        <a className="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
     </nav>
   );
 }
