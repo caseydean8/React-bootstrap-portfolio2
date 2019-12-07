637f159889ad6a99b68cf63f68a95313db2d0477
Casey Carroll <caseydean8@gmail.com>
2019-12-07 13:23:39 -0500

    added bootstrap js scripts to index.html to get collapsible navbar working

--- bootstrap-portfolio2/src/Components/Navbar.js
+++ bootstrap-portfolio2/src/Components/Navbar.js
@@ -8,25 +8,35 @@ function Navbar() {
       style={{ backgroundColor: "#c0c5cd" }}
     >
       <div class="container navbar-content">
-        <span class="navbar-brand mb-0 h1"><h1 style={{ borderBottom: "none" }}>Casey Carroll</h1></span>
+        <span class="navbar-brand mb-0 h1">
+          <h1 style={{ borderBottom: "none" }}>Casey Carroll</h1>
+        </span>
         <button
           class="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <Link class="nav-link" to="/">
-          About</Link>
-          <Link class="nav-link" to="/portfolio">
-          Portfolio</Link>
-          <Link class="nav-link" to="/contact">
-          Contact</Link>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div class="navbar-nav">
+            <Link class="nav-item nav-link active" to="/">
+              About
+            </Link>
+            <Link class="nav-item nav-link" to="/portfolio">
+              Portfolio
+            </Link>
+            <Link class="nav-item nav-link" to="/contact">
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
